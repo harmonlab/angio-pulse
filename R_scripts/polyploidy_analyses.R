31c5dd6f2e3f8b58514355d219ed233817e9f30f
Luke Harmon <lukeh@uidaho.edu>
2014-08-08 21:57:46 -0700

    updating binomial numbers to make sense

--- R_scripts/polyploidy_analyses.R
+++ R_scripts/polyploidy_analyses.R
@@ -1,4 +1,5 @@
 require(geiger)
+require(phylo)
 
 assess_nearness_in_rate_shift=function(phy, nodes, tips=TRUE){
 	cache=geiger:::.cache_tree(phy)
@@ -59,15 +60,16 @@ assess_nearness_in_rate_shift=function(phy, nodes, tips=TRUE){
 
 ## PLOT node distance from random nodes to nearest shift against polyploidization nodes from nearest shift
 ## USING MLE tree
-dat=get(load("~/Dropbox/angiospermsnescent/analyses/medusa/macgyver_turbo_0.92-3/results/spermatophyta_AToL_639_PL_MEDUSA_BATCH.effectsize.rda"))$summary
+setwd("~/Documents/tank/angiosperms/angio-pulse") # change to match local directory
+dat=get(load("spermatophyta_AToL_639_PL_MEDUSA_BATCH.effectsize.rda"))$summary
 dat=dat[which(dat$r>0),]
-phy=get(load("~/Dropbox/angiospermsnescent/analyses/medusa/macgyver_turbo_0.92-3/results/spermatophyta_AToL_639_PL_MEDUSA_BATCH.familial_MLE.rda"))$phy
+phy=get(load("spermatophyta_AToL_639_PL_MEDUSA_BATCH.familial_MLE.rda"))$phy
 
 nodes=match(rownames(dat), c(phy$tip.label, phy$node.label))
 
-rand_ndist=assess_nearness_in_rate_shift(phy, nodes)
+# rand_ndist=assess_nearness_in_rate_shift(phy, nodes)
 
-polyploidy=read.csv("polyploidization_events.csv", header=TRUE)
+polyploidy=read.csv("data_files/polyploidization_events.csv", header=TRUE)
 p_ndist=polyploidy$ndist
 p_ndist=p_ndist[!is.na(p_ndist)]
 
@@ -77,33 +79,36 @@ cat("INCLUDING TIP LINEAGES\n\n")
 # perfect correspondence
 x=sum(p_ndist==0)
 n=length(p_ndist)
-binom.test(x, n, p=sum(rand_ndist==0)/length(rand_ndist))
+#binom.test(x, n, p=sum(rand_ndist==0)/length(rand_ndist))
+# as in paper:
+
+binom.test(x, n, p=15/648)
 
 # delayed correspondence
 x=sum(p_ndist>0)
 n=length(p_ndist)
-binom.test(x, n, p=sum(rand_ndist>0)/length(rand_ndist))
+binom.test(x, n, p=34/323)
 
 ## EXCLUDING TIPS
 cat("EXCLUDING TIP LINEAGES\n\n")
-rand_tdist=assess_nearness_in_rate_shift(phy, nodes, tips=FALSE)
+#rand_tdist=assess_nearness_in_rate_shift(phy, nodes, tips=FALSE)
 
 t_ndist=polyploidy$ndist
 t_ndist=t_ndist[!is.na(t_ndist) & !polyploidy$tip]
 x=sum(t_ndist>0)
 n=length(t_ndist)
-binom.test(x, n, p=sum(rand_tdist>0)/length(rand_tdist))
+binom.test(x, n, p=34/323)
 
 
-pdf("internode_distance_to_nearest_shift.pdf")
-hist(p_ndist, breaks=c(-4,-2,0,2,4), include.lowest=TRUE, col=geiger:::.transparency("black", 0.85), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="")
-hist(rand_ndist, breaks=10,  col=geiger:::.transparency("lightgray", 1), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="", add=TRUE)
-legend("topright", legend=c("polyploidization", "random"), pt.bg=c(geiger:::.transparency("black", 0.85), geiger:::.transparency("lightgray", 1)), pch=22, bty="n")
-dev.off()
+# pdf("internode_distance_to_nearest_shift.pdf")
+# hist(p_ndist, breaks=c(-4,-2,0,2,4), include.lowest=TRUE, col=geiger:::.transparency("black", 0.85), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="")
+# hist(rand_ndist, breaks=10,  col=geiger:::.transparency("lightgray", 1), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="", add=TRUE)
+# legend("topright", legend=c("polyploidization", "random"), pt.bg=c(geiger:::.transparency("black", 0.85), geiger:::.transparency("lightgray", 1)), pch=22, bty="n")
+# dev.off()
 
 
-pdf("internode_distance_to_nearest_shift -- excluding tips.pdf")
-hist(t_ndist, breaks=c(-4,-2,0,2,4), include.lowest=TRUE, col=geiger:::.transparency("black", 0.85), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="")
-hist(rand_tdist, breaks=10,  col=geiger:::.transparency("lightgray", 1), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="", add=TRUE)
-legend("topright", legend=c("polyploidization", "random"), pt.bg=c(geiger:::.transparency("black", 0.85), geiger:::.transparency("lightgray", 1)), pch=22, bty="n")
-dev.off()
+# pdf("internode_distance_to_nearest_shift -- excluding tips.pdf")
+# hist(t_ndist, breaks=c(-4,-2,0,2,4), include.lowest=TRUE, col=geiger:::.transparency("black", 0.85), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="")
+# hist(rand_tdist, breaks=10,  col=geiger:::.transparency("lightgray", 1), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="", add=TRUE)
+# legend("topright", legend=c("polyploidization", "random"), pt.bg=c(geiger:::.transparency("black", 0.85), geiger:::.transparency("lightgray", 1)), pch=22, bty="n")
+# dev.off()
