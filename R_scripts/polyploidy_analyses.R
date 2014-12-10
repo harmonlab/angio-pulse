5b32294f6298d3a8251786c26727d26a8f45f999
lukejharmon <lukejharmon@gmail.com>
2014-12-09 22:36:32 -0800

    fixed polyploidy analysis

--- R_scripts/polyploidy_analyses.R
+++ R_scripts/polyploidy_analyses.R
@@ -1,114 +1,157 @@
 require(geiger)
 require(phylo)
 
-assess_nearness_in_rate_shift=function(phy, nodes, tips=TRUE){
-	cache=geiger:::.cache_tree(phy)
-	N=Ntip(phy)
-	n=Nnode(phy)
-	nn=N+n
-	others=(1:nn)[-(N+1)]
-    if(!tips) others=others[-c(1:N)]
-	dD=function(node){
-		if(node<=N) return(NA)
-		count=0
-		
-		zfun=function(n){
-			any(n%in%nodes)
-		}
-		
-		dd=node
-		while(1){
-			if(zfun(dd)) break()
-			count=count+1
-			dd=dd[dd>N]
-			if(!length(dd)){
-				count=NA
-				break()
-			}
-			dd=unlist(cache$fdesc[dd])
-		}
-		return(count)
-	}
-	
-	aA=function(node){
-		anc=cache$anc[[node]]
-		count=0
-		if(node%in%nodes) return(count)
-		count=NA
-		if(any(anc%in%nodes)){
-			count=-min(match(nodes, anc), na.rm=TRUE)
-		}
-		return(count)
-	}
 
-	tmp=lapply(others, function(x){
-        if(x%in%nodes) return(0)
-		aa=aA(x)
-		dd=dD(x)
-		f=c(aa, dd)
-		f=f[!is.na(f)]
-		if(!length(f)) return(NA)
-		if(length(unique(f))==1) return(f)
-		z=abs(c(dd,aa))
-		return(f[which(z==min(z))])
-	})
-	
-    ndists=unlist(tmp)
-	ndists=ndists[!is.na(ndists)]
-	return(ndists)
-}
-
-## PLOT node distance from random nodes to nearest shift against polyploidization nodes from nearest shift
 ## USING MLE tree
 setwd("~/Documents/tank/angiosperms/angio-pulse") # change to match local directory
 dat=get(load("spermatophyta_AToL_639_PL_MEDUSA_BATCH.effectsize.rda"))$summary
+
+# only consider upshifts
 dat=dat[which(dat$r>0),]
+
+# get the tree
 phy=get(load("spermatophyta_AToL_639_PL_MEDUSA_BATCH.familial_MLE.rda"))$phy
 
-nodes=match(rownames(dat), c(phy$tip.label, phy$node.label))
+shiftNode=match(rownames(dat), c(phy$tip.label, phy$node.label)) # nodes where shifts occur
 
-# rand_ndist=assess_nearness_in_rate_shift(phy, nodes)
-
-polyploidy=read.csv("data_files/polyploidization_events.csv", header=TRUE)
-p_ndist=polyploidy$ndist
-p_ndist=p_ndist[!is.na(p_ndist)]
-
-## INCLUDING TIPS
-cat("INCLUDING TIP LINEAGES\n\n")
-## BINOMIAL TESTS
-# perfect correspondence
-x=sum(p_ndist==0)
-n=length(p_ndist)
-#binom.test(x, n, p=sum(rand_ndist==0)/length(rand_ndist))
-# as in paper:
-
-binom.test(x, n, p=15/648)
-
-# delayed correspondence
-x=sum(p_ndist>0)
-n=length(p_ndist)
-binom.test(x, n, p=34/323)
-
-## EXCLUDING TIPS
-cat("EXCLUDING TIP LINEAGES\n\n")
-#rand_tdist=assess_nearness_in_rate_shift(phy, nodes, tips=FALSE)
-
-t_ndist=polyploidy$ndist
-t_ndist=t_ndist[!is.na(t_ndist) & !polyploidy$tip]
-x=sum(t_ndist>0)
-n=length(t_ndist)
-binom.test(x, n, p=34/323)
+# polyploidy events from tank
+polyploidy=read.csv("data_files/Polyploidization.Dec2014.LJH.csv", header=TRUE)
 
 
-# pdf("internode_distance_to_nearest_shift.pdf")
-# hist(p_ndist, breaks=c(-4,-2,0,2,4), include.lowest=TRUE, col=geiger:::.transparency("black", 0.85), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="")
-# hist(rand_ndist, breaks=10,  col=geiger:::.transparency("lightgray", 1), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="", add=TRUE)
-# legend("topright", legend=c("polyploidization", "random"), pt.bg=c(geiger:::.transparency("black", 0.85), geiger:::.transparency("lightgray", 1)), pch=22, bty="n")
-# dev.off()
+# this returns all combinations of alternate placements for 
+# the polyploidy events - there are 6 x 2 x 3 = 36 combinations
+return_all_combinations<-function(polyploidy) {
+	res<-list()
+	counter<-1
+	for(i in 3:8) # loop through WGD 6
+		for(j in 11:12) # loop through WGD 6
+			for(k in 15:17) { # loop through WGD 9 
+				rowSelections<-c(1, 2, i, 9, 10, j, 13, 14, k)
+				res[[counter]]<-polyploidy[rowSelections,]
+				counter <- counter + 1
+			}
+	
+	res			
+}
 
+# get them
+wgdComb<-return_all_combinations(polyploidy)
 
-# pdf("internode_distance_to_nearest_shift -- excluding tips.pdf")
-# hist(t_ndist, breaks=c(-4,-2,0,2,4), include.lowest=TRUE, col=geiger:::.transparency("black", 0.85), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="")
-# hist(rand_tdist, breaks=10,  col=geiger:::.transparency("lightgray", 1), freq=FALSE, xlim=c(-20,20), xlab="internode distance", ylab="density", main="", add=TRUE)
-# legend("topright", legend=c("polyploidization", "random"), pt.bg=c(geiger:::.transparency("black", 0.85), geiger:::.transparency("lightgray", 1)), pch=22, bty="n")
-# dev.off()
+# randomly assign n polyploidy events to branches in the tree -
+# this is the null model
+randomPolyploidyNodes<-function(phy, n) {
+	sample(phy$edge[,2], size=n, replace=FALSE)
+}
+
+# test for exact matches
+exactMatches<-numeric(length(wgdComb))
+randomExactMatches<-matrix(nrow=1000, ncol=length(wgdComb))
+
+# loop through all combinations
+for(i in 1:length(wgdComb)) {
+	
+	# get the ith one
+	pp<-wgdComb[[i]]
+	
+	# left and right tips used to get the nodes
+	pl<-as.character(pp$left_tip)
+	pr<-as.character(pp$right_tip)
+
+	polyNode<-numeric(length=nrow(pp))
+	for(j in 1:nrow(pp)) {
+		if(pl[j]==pr[j]) { # this is a tip
+			ww<-which(phy$tip.label==pl[j])
+			xx<-which(phy$edge[,2]==ww)
+			polyNode[j]<-phy$edge[xx,1]
+		} else { # this is an internal node
+			pair<-c(pl[j], pr[j])
+			polyNode[j]<-getMRCA(phy, pair)
+		}
+	}
+	
+	exactMatches[i]<-sum(!is.na(match(polyNode, shiftNode))) # count exact matches
+	
+	for(j in 1:1000) { # repeat for 1000 random assignments of polyploidy
+		randomShifts<-randomPolyploidyNodes(phy, length(shiftNode))
+		randomExactMatches[j,i] <- sum(!is.na(match(polyNode, randomShifts)))
+	}
+}
+
+# calculate p-values
+pExact<-numeric(length(exactMatches)) 
+for(i in 1:length(exactMatches)) {
+	pExact[i]<-(sum(randomExactMatches[,i] >= exactMatches[i])+1)/1001
+}
+
+# six are significant
+pExact < 0.05
+	
+# now repeat using downstream distances
+
+# test for exact matches
+tipDistances<-numeric(length(wgdComb))
+randomTipDistances <-matrix(nrow=1000, ncol=length(wgdComb))
+
+#tcache<-geiger:::.cache.descendants(phy)
+
+library(phytools)
+p2<-phy
+p2$edge.length[]<-1
+lookupDistance<-dist.nodes(p2)
+
+closeCount<-function(pn, sn, cutoff=3) {
+	
+	mdists<-numeric(length(pn))
+	# gather descendants of poly nodes pn
+	for(i in 1:length(pn)) {
+		pd<-getDescendants(phy, pn[i])
+		targets<-pd[which(pd %in% sn)]
+		targetDistance<-lookupDistance[pn[i],targets]
+		if(length(targetDistance)!=0) {
+			mdists[i]<-min(targetDistance)
+		} else mdists[i]<-Inf
+	}
+	testStat<-sum(mdists <= cutoff)
+	testStat
+}
+
+# test for close matches
+testStat<-numeric(length(wgdComb))
+nullDist<-matrix(nrow=1000, ncol=length(wgdComb))
+
+# loop through all combinations
+for(i in 17:length(wgdComb)) {
+	
+	# get the ith one
+	pp<-wgdComb[[i]]
+	
+	# left and right tips used to get the nodes
+	pl<-as.character(pp$left_tip)
+	pr<-as.character(pp$right_tip)
+
+	polyNode<-numeric(length=nrow(pp))
+	for(j in 1:nrow(pp)) {
+		if(pl[j]==pr[j]) { # this is a tip
+			ww<-which(phy$tip.label==pl[j])
+			xx<-which(phy$edge[,2]==ww)
+			polyNode[j]<-phy$edge[xx,1]
+		} else { # this is an internal node
+			pair<-c(pl[j], pr[j])
+			polyNode[j]<-getMRCA(phy, pair)
+		}
+	}
+	
+	testStat[i]<-closeCount(polyNode, shiftNode)
+		
+	for(j in 1:1000) { # repeat for 1000 random assignments of polyploidy
+		randomShifts<-randomPolyploidyNodes(phy, length(shiftNode))
+		nullDist[j,i] <- closeCount(polyNode, randomShifts)
+	}
+}
+
+# calculate p-values
+pClose<-numeric(length(testStat)) 
+for(i in 1:length(testStat)) {
+	pClose[i]<-(sum(nullDist[,i] >= testStat[i])+1)/1001
+}
+
